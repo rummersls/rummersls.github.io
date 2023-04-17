b51936a8f80115131d8a2485d77c6a82f319b5b3
rummersls <130941631+rummersls@users.noreply.github.com>
2023-04-17 01:28:59 +0100

    Initial commit
    Uploading site files

---
+++ js/custom.js
@@ -0,0 +1,115 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+
+
+getYear();
+
+function backgroundImageSlider(){
+    
+}
+
+
+var numberOfImagesInDirectory = 20;
+
+
+var bgImageArray = [],
+base = "images/gallery/",
+secs = 6;
+
+for (let i = 1; i < numberOfImagesInDirectory; i++) {
+    new Image().src = base + i + ".jpg"; 
+    bgImageArray.push(i + ".jpg");
+}
+
+
+// Custom Rummers Background JS L Stone 13/03/2023
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.body.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.body.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+// isotope js
+$(window).on('load', function () {
+    backgroundSequence();
+
+    
+    
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: data
+        })
+    });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
+});
+
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
